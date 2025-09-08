5daade43c74b37095d7c7020cff8e59977f7c224
JPMadruga01 <jp.madruga.ff@gmail.com>
2025-09-08 17:02:37 -0300

    arrumando

--- project-name/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-
-
-@Module({ // função que adiciona alguns metadados para a classe
-  imports: [AuthModule],
-})
-export class AppModule {}
